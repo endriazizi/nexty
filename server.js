8a833eba3d4ae13e7c3df47a779e6e90cd21c38c
endriazizi <endri.azizi@gmail.com>
2021-04-11 02:56:52 +0200

    ok

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/nexty'));
+app.use(express.static('./dist/nexty-app'));
 
 app.get('/*', (req, res) =>
-    res.send('hello')
-    // res.sendFile('index.html', {root: 'dist/nexty/'}),
+     res.sendFile('index.html', {root: 'dist/nexty-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
